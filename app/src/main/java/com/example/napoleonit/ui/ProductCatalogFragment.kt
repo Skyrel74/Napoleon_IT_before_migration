1d29c62d21bb02a4ebf2fa4b01b8f619a7813321
Ilya Rakipov <ilya.rakipov@gmail.com>
2020-12-12 16:02:23 +0500

    Data exchange through fragments fix

--- app/src/main/java/com/example/napoleonit/ui/ProductCatalogFragment.kt
+++ app/src/main/java/com/example/napoleonit/ui/ProductCatalogFragment.kt
@@ -3,6 +3,7 @@ package com.example.napoleonit.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.example.napoleonit.Product
 import com.example.napoleonit.R
 import kotlinx.android.synthetic.main.fragment_product_catalog.*
 
@@ -11,9 +12,11 @@ class ProductCatalogFragment : Fragment(R.layout.fragment_product_catalog) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val product = Product("Мяч", 800.0)
+
         detailedBtn.setOnClickListener {
             requireFragmentManager().beginTransaction()
-                    .replace(R.id.container, ProductDetailsFragment())
+                    .replace(R.id.container, ProductDetailsFragment.newInstance(product))
                     .addToBackStack("Catalog")
                     .commit()
         }
--- app/src/main/java/com/example/napoleonit/ui/ProductDetailsFragment.kt
+++ app/src/main/java/com/example/napoleonit/ui/ProductDetailsFragment.kt
@@ -1,6 +1,33 @@
 package com.example.napoleonit.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.example.napoleonit.Product
 import com.example.napoleonit.R
+import kotlinx.android.synthetic.main.fragment_product_details.*
 
-class ProductDetailsFragment : Fragment(R.layout.fragment_product_details)
+class ProductDetailsFragment : Fragment(R.layout.fragment_product_details) {
+
+    companion object {
+
+        private const val PRODUCT_TAG = "PRODUCT_TAG"
+
+        fun newInstance(product: Product) =
+                ProductDetailsFragment().apply {
+                    arguments = Bundle().apply {
+                        putParcelable(PRODUCT_TAG, product)
+                    }
+                }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            val product = it.getParcelable<Product>(PRODUCT_TAG)
+
+            textView.text = "${textView.text}\n${product?.name} ${product?.price}"
+        }
+    }
+}
